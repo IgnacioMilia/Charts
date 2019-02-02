ec469b2ba24bf6b9b586afe22c033584846d90d7
Ignacio Milia <ignaciomilia91@gmail.com>
2019-02-02 15:48:29 -0300

    Todos los gráficos muestran algo

--- src/charts/Controller.java
+++ src/charts/Controller.java
@@ -7,11 +7,9 @@ import javafx.scene.chart.*;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
-// 
 
 public class Controller {
-    private ObservableList<XYChart.Series> slist;
-    private ObservableList<XYChart.Data> dlist;
+    private ObservableList<PieChart.Data> dlist;
     
 // Gráficos
     private @FXML BarChart barChart;
@@ -20,58 +18,58 @@ public class Controller {
     private @FXML AreaChart areaChart;
     private @FXML StackedAreaChart stockedAreaChart;
     
-    
     // Tabs
     @FXML Tab tabBar;
     @FXML Tab tabLine;
     @FXML Tab tabPie;
     @FXML Tab tabArea;
     @FXML Tab tabStockedArea;
-    
     @FXML Label lbl;
-    private PieChart.Data pieData;
     
-    private XYChart.Series series1;
-    private XYChart.Series series2;
+    private PieChart.Data pieData;
+    private Series series1;
+    private Series series2;
+    private Series series3;
     
     public void setData() {
+        
         dlist = FXCollections.observableArrayList();
         
         series1 = new Series();
         series2 = new Series();
-        Series series3 = new Series();
+        series3 = new Series();
         
-        
-        for (int i = -5000; i < 5000; i += 500) {
-            series1.getData().add(new XYChart.Data(""+i, 3*i*i - 100*i +30));
+        for (int i = -5000; i < 5000; i += 100) {
+            series1.getData().add(new XYChart.Data(i*i*i - i*i, 3*i*i - 100*i +30));
             series2.getData().add(new XYChart.Data(""+i, i*i*i - i*i));
             series3.getData().add(new XYChart.Data(""+i, 3*(i*i*i) + i*i +400));
-            //dlist.add(new XYChart.Data("assss", -3 * (i*i*i) + 5 * (i*i) - 1 ));
-           //dlist.add(new XYChart.Data("assss2", 3 * (i*i*i) - 5 * (i*i) - 1 ));
+           
+            dlist.clear();
+            dlist.add(new PieChart.Data("1", 3*i*i - 100*i +30 ));
+            dlist.add(new PieChart.Data("2", i*i*i - i*i ));
+            dlist.add(new PieChart.Data("3", 3*(i*i*i) + i*i +400));
         }
-        
-//        ObservableList datos = FXCollections.observableArrayList(dlist);
-        // cómo agregar más funciones?
-        //ObservableList datos2 = FXCollections.observableArrayList( datos);
-        
-//        lineChart.
+
         switch(lbl.getText()){
             case "barras":
                 barChart.getData().clear();
-                barChart.getData().addAll(series1, series2, series3);
+                barChart.getData().addAll( series2, series3);
                 break;
             case "lineas":
                 lineChart.getData().clear();
-                lineChart.getData().addAll(series1, series2, series3);
+                lineChart.getData().addAll( series2, series3);
                 break;
             case "pie":
+                pieChart.setData(dlist);
                 // ...
                 break;
             case "area":
                 areaChart.getData().clear();
-                areaChart.getData().addAll(series1, series2, series3);
+                areaChart.getData().addAll( series2, series3);
                 break;
             case "stkArea":
+                stockedAreaChart.setData(FXCollections.observableArrayList(series1));
+//                stockedAreaChart.getData().addAll(series1,series2,series3);
                 // ...
                 break;
         }
